b4481bf48df26e032212814dc08dda22f95583ac
Vlad Shilov <omgovich@ya.ru>
2019-09-18 12:42:43 +0300

    Refactor to ES6

---
+++ index.js
@@ -0,0 +1,48 @@
+export const generatePassword = function(options = {}) {
+  return getRandomPassword({
+    syllablesCount: 3,
+    minSyllableLength: 2,
+    maxSyllableLength: 3,
+    hasNumbers: true,
+    titlecased: true,
+    vowels: "aeiouy",
+    consonants: "bcdfghklmnprstvz",
+    ...options
+  });
+};
+
+const getRandomPassword = ({ syllablesCount, ...settings }) => {
+  return times(syllablesCount, () => getRandomSyllable(settings)).join("");
+};
+
+const getRandomSyllable = ({
+  consonants,
+  vowels,
+  hasNumbers,
+  titlecased,
+  minSyllableLength,
+  maxSyllableLength
+}) => {
+  const length = getRandomNumber(minSyllableLength, maxSyllableLength);
+
+  const syllable = times(length, index => {
+    const char = getRandomChar(index % 2 ? vowels : consonants);
+    if (index === 0 && titlecased) return char.toUpperCase();
+    return char;
+  }).join("");
+
+  if (hasNumbers) return syllable + getRandomNumber(0, 9);
+  return syllable;
+};
+
+const getRandomChar = stack => {
+  return stack.charAt(getRandomNumber(0, stack.length - 1));
+};
+
+const getRandomNumber = (from, to) => {
+  return Math.round(Math.random() * (to - from) + from);
+};
+
+const times = (number, callback) => {
+  return Array(number).map((_, index) => callback(index));
+};
