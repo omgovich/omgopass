7d4616469d02fe2c31e8eff0baea9912621baec4
Vlad Shilov <omgovich@ya.ru>
2019-09-22 20:30:16 +0300

    Add copy button

--- src/main.js
+++ src/main.js
@@ -2,16 +2,28 @@ import "./styles.css";
 import { generatePassword } from "omgopass";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const $password = document.getElementsByClassName("password")[0];
+  const $password = document.getElementsByName("password")[0];
   const $button = document.getElementsByClassName("button")[0];
+  const $copyButton = document.getElementsByClassName("password__copy")[0];
   const $numbers = document.getElementsByName("numbers")[0];
   const $titlecase = document.getElementsByName("titlecase")[0];
 
   const renderPassword = () => {
-    $password.innerHTML = generatePassword({
+    $password.value = generatePassword({
       hasNumbers: $numbers.checked,
       titlecased: $titlecase.checked
     });
+  };
+
+  const copyToClipboard = () => {
+    $password.select();
+    document.execCommand("copy");
+
+    if (document.selection) {
+      document.selection.empty();
+    } else if (window.getSelection) {
+      window.getSelection().removeAllRanges();
+    }
   };
 
   renderPassword();
@@ -19,4 +31,9 @@ document.addEventListener("DOMContentLoaded", () => {
   $button.addEventListener("click", renderPassword);
   $numbers.addEventListener("change", renderPassword);
   $titlecase.addEventListener("change", renderPassword);
+  $copyButton.addEventListener("click", copyToClipboard);
+
+  $password.addEventListener("click", () => {
+    $password.select();
+  });
 });
