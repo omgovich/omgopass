04677ef5a519f85c9f4b9fc8d58fbb05b9bcddfb
Vlad Shilov <omgovich@ya.ru>
2019-09-22 20:36:22 +0300

    Updates

--- main.1e43358e.js
+++ main.1e43358e.js
@@ -321,7 +321,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57952" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61044" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
