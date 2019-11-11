899ff9914f69d1ab5021e2bcea775bf11dc679a5
Vlad Shilov <omgovich@ya.ru>
2019-11-11 23:32:31 +0300

    Improve the way to get a random number

--- random.browser.js
+++ random.browser.js
@@ -1,12 +1,7 @@
 const crypto = self.crypto || self.msCrypto;
 
-module.exports = (from, to) => {
-  const range = to - from;
+module.exports = limit => {
+  if (crypto) return crypto.getRandomValues(new Uint8Array(1))[0] % limit;
 
-  if (crypto) {
-    const randomNumber = crypto.getRandomValues(new Uint8Array(1))[0];
-    return from + (randomNumber % range);
-  }
-
-  return from + Math.round(Math.random() * range);
+  return Math.round(Math.random() * 0xffff) % limit;
 };
