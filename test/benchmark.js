3761109253ecd2dc6ca0d009a5cf0d4bb091b81e
Vlad Shilov <omgovich@ya.ru>
2019-11-13 22:21:33 +0300

    Benchmark update

--- test/benchmark.js
+++ test/benchmark.js
@@ -3,12 +3,15 @@ const omgopass = require("../");
 const passwordGenerator = require("password-generator");
 const generatePassword = require("generate-password");
 const niceware = require("niceware");
+const xkpasswd = require("xkpasswd");
+const secureRandomPassword = require("secure-random-password");
 
 const suite = new Benchmark.Suite();
 
 const libraries = [
   {
     name: "omgopass",
+    memorable: true,
     browser: true,
     node: true,
     size: 334,
@@ -16,6 +19,7 @@ const libraries = [
   },
   {
     name: "password-generator",
+    memorable: true,
     browser: true,
     node: true,
     size: 644,
@@ -23,23 +27,43 @@ const libraries = [
   },
   {
     name: "generate-password",
+    memorable: false,
     browser: false,
     node: true,
     size: 740,
     generate: () => {
       generatePassword.generate({
         length: 12,
+        symbols: false,
         uppercase: true,
-        numbers: true
+        numbers: true,
+        strict: true
       });
     }
   },
   {
+    name: "secure-random-password",
+    memorable: false,
+    browser: true,
+    node: true,
+    size: 8939,
+    generate: () => secureRandomPassword.randomPassword()
+  },
+  {
     name: "niceware",
+    memorable: true,
     browser: true,
     node: true,
     size: 191 * 1024,
     generate: () => niceware.generatePassphrase(8)
+  },
+  {
+    name: "xkpasswd",
+    memorable: true,
+    browser: false,
+    node: true,
+    size: 715 * 1024,
+    generate: () => xkpasswd()
   }
 ];
 
@@ -52,14 +76,15 @@ libraries.forEach(library => {
 suite
   .on("cycle", event => {
     const { name, hz } = event.target;
-    const library = libraries.find(lib => lib.name === name);
+    const { size, generate, ...library } = libraries.find(
+      lib => lib.name === name
+    );
 
     results.push({
       name,
       "ops/sec": Math.round(hz),
-      "size (bytes)": library.size,
-      browser: library.browser,
-      node: library.node
+      "size (bytes)": size,
+      ...library
     });
   })
   .on("complete", () => {
