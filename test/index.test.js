e51932822e82fc111cf298f56687d67182e3822d
Vlad Shilov <omgovich@ya.ru>
2019-11-14 17:49:23 +0300

    Estimate password strength by Dropbox/zxcvbn

--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,24 @@
 const generatePassword = require("../");
+const zxcvbn = require("zxcvbn");
+
+test("password strength", () => {
+  // See zxcvbn docs: https://github.com/dropbox/zxcvbn#usage
+  const { score } = zxcvbn(generatePassword());
+
+  // 0 # too guessable: risky password. (guesses < 10^3)
+  // 1 # very guessable: protection from throttled online attacks. (guesses < 10^6)
+  // 2 # somewhat guessable: protection from unthrottled online attacks. (guesses < 10^8)
+  // 3 # safely unguessable: moderate protection from offline slow-hash scenario. (guesses < 10^10)
+  expect(score).toBeGreaterThanOrEqual(3);
+});
+
+test("long password strength", () => {
+  const password = generatePassword({ syllablesCount: 4 });
+  const { score } = zxcvbn(password);
+
+  // 4 # very unguessable: strong protection from offline slow-hash scenario. (guesses >= 10^10)
+  expect(score).toBeGreaterThanOrEqual(4);
+});
 
 test("default settings", () => {
   const password = generatePassword();
